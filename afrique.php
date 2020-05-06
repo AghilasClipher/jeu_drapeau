63bd442e52f39b504ecd392cadaee4c8c93a5abc
Aghilas_Kessaï <56322894+AghilasClipher@users.noreply.github.com>
2020-05-07 01:28:44 +0200

    Modification des msg d'erreurs sign up

--- afrique.php
+++ afrique.php
@@ -27,8 +27,6 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     
     <?php
-      //nouveau commentaire
-       //commentaire php ! 
         include_once 'fct.php';
          init_session();
          include_once 'connexionphp.php';
