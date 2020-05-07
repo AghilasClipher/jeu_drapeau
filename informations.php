ab121fbb9db4bfa17880fc8650e66bd2a1f53328
Aghilas_Kessaï <56322894+AghilasClipher@users.noreply.github.com>
2020-05-07 03:37:01 +0200

    ajout page profil

--- informations.php
+++ informations.php
@@ -1,3 +1,11 @@
+<?php 
+    require 'fct.php';
+    init_session();
+    //On ne peut pas accéder à la page du profil si on est pas connecté. 
+    if(!is_logged()){
+        header('Location:index.php');
+    }
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -9,7 +17,10 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" />
-		<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"></script>
+    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"></script>
+    
+    <!-- Feuille de style CSS-->
+    <link rel="stylesheet" href=css/informations.css>
   </head>
   <body>
       
@@ -19,6 +30,31 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     
+    <div id="bloc_profil">
+      <div id="sous_bloc">
+        <h1> Votre profil </h1>
+        <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+          <li class="nav-item">
+            <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Informations</a>
+          </li>
+          <li class="nav-item">
+          <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Statistiques</a>
+          </li>
+        </ul>
+        <div class="tab-content " id="pills-tabContent">
+          <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+            <p> Informations</p>
+          </div>
+          <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+            <p> Statistiques </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
 
     </body>
 </html>
